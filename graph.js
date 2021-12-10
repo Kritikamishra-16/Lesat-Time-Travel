fa004ed952718a48081c939ce1248e7d087e336f
Kritika Mishra <mishrakritika2001@gamil.com>
2021-12-10 23:03:33 +0530

    initial commit

---
+++ graph.js
@@ -0,0 +1,16 @@
+ function createGraph(V,E){
+    let adj_list = []; 
+    for(let i = 0 ; i < V ; i++){
+        adj_list.push([]);
+    }
+    for(let i = 0 ; i < E.length ; i++){
+        adj_list[E[i][0]].push([E[i][1],E[i][2]]);
+        adj_list[E[i][1]].push([E[i][0],E[i][2]]);
+    }
+    return adj_list;
+}
+
+let V = 3;
+let E = [[0,1,1],[0,2,2],[1,2,3]];
+let graph = createGraph(V,E);
+console.log(graph);
